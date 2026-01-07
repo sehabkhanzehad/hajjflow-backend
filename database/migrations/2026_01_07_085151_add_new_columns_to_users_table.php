b2e3b447b8fa111af0510cc51663048ac853b886
Sk Zehad <sehabkhanzehad@yahoo.com>
2026-01-07 17:51:00 +0600

    feat: Add occupation and spouse_name columns to users table and remove address column

--- database/migrations/2026_01_07_085151_add_new_columns_to_users_table.php
+++ database/migrations/2026_01_07_085151_add_new_columns_to_users_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('user', function (Blueprint $table) {
+        Schema::table('users', function (Blueprint $table) {
             $table->string('occupation')->nullable()->after('password');
             $table->string('spouse_name')->nullable()->after('father_name_bangla');
             $table->dropColumn('address');
@@ -23,7 +23,7 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('user', function (Blueprint $table) {
+        Schema::table('users', function (Blueprint $table) {
             $table->dropColumn('occupation');
             $table->dropColumn('spouse_name');
             $table->string('address')->nullable()->after('date_of_birth');
