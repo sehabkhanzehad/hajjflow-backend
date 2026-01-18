fbb45fc5d230e1728854304e8708a28fe0272645
Sk Zehad <sehabkhanzehad@yahoo.com>
2026-01-18 17:17:27 +0600

    feat: Add boolean cast for is_married attribute in User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -24,6 +24,7 @@ class User extends Authenticatable implements MustVerifyEmail
     protected $casts = [
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
+        'is_married' => 'boolean',
     ];
 
     protected $guarded = ['id'];
